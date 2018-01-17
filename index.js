e5b06d2220d15dc183c8f51d6a856ccd0b13cd08
Arthur Lurye <arthurlurye@gmail.com>
2018-01-17 23:39:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,7 @@ const facts = [
 ];
 
 function johnLennonFacts(facts){
-  const facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-  ];
+
 
   const LennonFacts = [];
    let i = 0;

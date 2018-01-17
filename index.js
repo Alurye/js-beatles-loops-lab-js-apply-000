014c7064bb511b148cb789939cf40fe7f56a7787
Arthur Lurye <arthurlurye@gmail.com>
2018-01-17 23:11:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+const Beatles = [];
+  for (var i = 0; i < muscians.length; i++) {
+    Beatles.push[musicians[i] + " plays " + instruments[i]].toString();
+  }
+  return Beatles;
+}

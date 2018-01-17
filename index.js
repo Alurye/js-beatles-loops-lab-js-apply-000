adf5cce19233b150813469d5053a5883d0c18ca2
Arthur Lurye <arthurlurye@gmail.com>
2018-01-17 23:33:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 function theBeatlesPlay(musicians, instruments){
 var Beatles = [];
@@ -16,4 +10,19 @@ var Beatles = [];
   return Beatles;
 }
 
+function johnLennonFacts(facts){
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ];
 
+  const LennonFacts = [];
+   let i = 0;
+   while (i < facts.length) {
+     LennonFacts.push(facts[i] + "!!!");
+     i++;
+   }
+   return LennonFacts;
+}
